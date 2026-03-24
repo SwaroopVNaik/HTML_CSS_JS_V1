b83780a5ed639cb3297e75f3bca9f41b8874e815
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-03-24 16:05:04 +0530

    Python

---
+++ one.py
@@ -0,0 +1,13 @@
+# import keyword
+# print(keyword.kwlist)
+# print(len(keyword.kwlist))
+
+eid = 101
+ename = "rahul"
+esal = 45000.05
+rating = 4.3 
+
+print(eid)
+print(ename)
+print(esal)
+print(rating)
