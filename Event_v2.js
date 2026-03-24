78b12212da3b341a206a759464f92e7bac1da590
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-03-24 16:04:06 +0530

    JS , HTML

---
+++ Event_v2.js
@@ -0,0 +1,10 @@
+function changeColor5()
+{
+    document.getElementById("ip1").style.backgroundColor = "yellow";
+}
+
+function convertUpper()
+{
+    var text = document.getElementById("ip2").value;  // Value is a property
+    document.getElementById("ip2").value = text.toUpperCase();
+}
---
+++ fourty.js
@@ -0,0 +1,10 @@
+// loosly typed
+var eid = 101;
+var ename = "rahul"
+var esal = 45000.00 
+var rating = 4.3 
+
+console.log(eid);
+console.log(ename);
+console.log(esal);
+console.log(rating);
