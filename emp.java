c3002edb72f5334f3ed256bc6e1bd57989e30494
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-03-24 16:04:29 +0530

    Java

---
+++ emp.java
@@ -0,0 +1,15 @@
+// java is stricly and static type programming language
+// here we have primitive data type and refernce datatype
+public class emp{
+    public static void main(String[] args) {
+        int eid = 101;
+        String ename = "swaroop";
+        double esal = 45000.34;
+        float rating = 4.5f;
+
+        System.out.println(eid);
+        System.out.println(ename);
+        System.out.println(esal);
+        System.out.println(rating);
+    }
+} 
