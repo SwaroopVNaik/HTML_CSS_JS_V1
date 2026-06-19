b31aac61e1e420c23bd42cda2b0eaf8d48c38a89
SwaroopVNaik <swaroop.v.naik@gmail.com>
2026-06-19 13:48:58 +0530

    done

---
+++ practice.js
@@ -0,0 +1,28 @@
+// Array Destructring 
+
+// const arr = ["Swaroop", 22];
+// const [name, age] = arr;
+
+// console.log(name);
+// console.log(age);
+
+// const arr_1 = [10, 20, 30]
+
+// const [a, , c] = arr_1;
+
+// console.log(a);
+// console.log(c);
+
+
+// Object Destructring 
+
+const user = {
+    name : "Swaroop",
+    age : 22
+};
+
+const {name , age } = user;
+
+console.log(name);
+console.log(age);
+
